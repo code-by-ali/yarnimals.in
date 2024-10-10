afcc923159e9e3eb025bac5b9ff66c7b3080eaf2
Mohammed Ali <mohammed.ali@rzolut.com>
2024-10-10 11:33:07 +0530

    header changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,11 +5,12 @@ import { UserOutlined } from "@ant-design/icons";
 
 const Header = () => {
 	return (
-		<header className="flex items-center justify-between px-3 py-3 border-b border-[#ddd]">
-			<div className="flex items-center gap-2">
+		<header className="sticky top-0 bg-white flex items-center justify-between px-3 py-2.5 border-b border-[#ddd]">
+			<div className="flex items-center justify-between gap-2">
 				<Logo1 className="h-8 w-auto" />
-				<Logo2 className="h-8 w-44" />
+				{/* <Logo2 className="h-8 w-44" /> */}
 			</div>
+			<span className="text-blue1 font-bold text-lg">YARNIMALS.IN</span>
 			<button className="flex items-center text-[21px] justify-center bg-blue1 text-white border-none h-9 w-9 rounded-lg">
 				<UserOutlined />
 			</button>
