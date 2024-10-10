64870ea12af3c2a3f1227f8f115d360ee0a80b98
Mohammed Ali <mohammed.ali@rzolut.com>
2024-10-10 11:41:35 +0530

    Logo 2 removed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { ReactComponent as Logo1 } from "../../logo1.svg";
-import { ReactComponent as Logo2 } from "../../logo-2.svg";
 import { UserOutlined } from "@ant-design/icons";
 
 const Header = () => {
@@ -8,7 +7,6 @@ const Header = () => {
 		<header className="sticky top-0 bg-white flex items-center justify-between px-3 py-2.5 border-b border-[#ddd]">
 			<div className="flex items-center justify-between gap-2">
 				<Logo1 className="h-8 w-auto" />
-				{/* <Logo2 className="h-8 w-44" /> */}
 			</div>
 			<span className="text-blue1 font-bold text-lg">YARNIMALS.IN</span>
 			<button className="flex items-center text-[21px] justify-center bg-blue1 text-white border-none h-9 w-9 rounded-lg">
